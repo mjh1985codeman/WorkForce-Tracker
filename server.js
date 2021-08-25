f2a1ee1f8f8ecc407bec3d8fd5484b526d7fe2d1
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-25 17:34:53 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,29 @@
+//importing connection.js file.
+const db = require("./db/connection");
+const express = require("express");
+// Add near the top of the file
+const inquirer = require("inquirer");
+const apiRoutes = require("./routes/apiRoutes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// Add after Express middleware
+app.use("/api", apiRoutes);
+
+// Default response for any other request (Not Found) this always needs to be the LAST route.
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected.");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
