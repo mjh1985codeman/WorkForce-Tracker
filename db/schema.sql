ed389507893f2399c6e8279d607658817456d65f
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-25 19:00:42 -0500

    add database and tables in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS workforce_tracker_db;
+CREATE DATABASE workforce_tracker_db;
+USE workforce_tracker_db;
+-- 
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+--
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2),
+    department_id INTEGER,
+    FOREIGN KEY (id) REFERENCES departments (id)
+);
+--
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employees (id)
+);
