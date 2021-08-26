fc3df6e35f24632ba34837e847a5076ddc9ae4e5
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-25 22:12:42 -0500

    got employees GET and POST route complete

--- routes/apiRoutes/employees.js
+++ routes/apiRoutes/employees.js
@@ -16,3 +16,23 @@ router.get("/employees", (req, res) => {
     });
   });
 });
+
+//Create a employee
+router.post("/employee", ({ body }, res) => {
+  const sql = `INSERT INTO employees (first_name, last_name, role_id)
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.role_id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
+
+module.exports = router;
