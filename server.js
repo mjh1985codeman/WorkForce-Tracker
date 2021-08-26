120c9e5261e243e37db64921c6fc5ddab7576070
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-26 13:20:28 -0500

    Began inquirer prompt work

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const db = require("./db/connection");
 const express = require("express");
 // Add near the top of the file
-// const inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3001;
@@ -19,6 +20,58 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
+// Question Variables.
+// First Questions that user is prompted with.
+const firstQuestions = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    name: "firstResponse",
+    choices: [
+      "View All Departments",
+      "View All Roles",
+      "View All Employees",
+      "Add a Department",
+      "Add a Role",
+      "Add a Employee",
+      "Update Employee Role",
+    ],
+  },
+];
+
+//Options that require additional Input.
+const addDeptPrompts = [
+  {
+    type: "input",
+    message: "Enter Department Name",
+    name: "newDept",
+  },
+];
+
+const addRolePrompts = [
+  {
+    type: "input",
+    message: "Enter new Employees first name",
+    name: "newEmpFirstName",
+  },
+  {
+    type: "input",
+    message: "Enter new Employees last name",
+    name: "newEmpLastName",
+  },
+  {
+    type: "list",
+    message: "Choose this employee's Role",
+    name: "newEmpRole",
+    //Should this be an empty array as the choices will depend on the Roles that the user adds?
+    choices: [],
+  },
+];
+
+function initApp() {
+  inquirer.prompt(firstQuestions);
+}
+
 // Start server after DB connection
 db.connect((err) => {
   if (err) throw err;
@@ -27,3 +80,5 @@ db.connect((err) => {
     console.log(`Server running on port ${PORT}`);
   });
 });
+
+initApp();
