396030dc2b985d3cec0f1cd5fd830178eabb0850
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-27 09:27:08 -0500

    seeded employees table

--- db/schema.sql
+++ db/schema.sql
@@ -22,9 +22,8 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    manager_id INTEGER
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employees (id)
+    ON DELETE SET NULL
 );
-
--- FOREIGN KEY (role_id) REFERENCES roles (id)
-
--- FOREIGN KEY (manager_id) REFERENCES employees (id)
